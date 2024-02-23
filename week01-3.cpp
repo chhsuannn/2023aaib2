a18c1bc5860c54d7bd5dd82361190007582ac4f2
chhsuannn <145088556+chhsuannn@users.noreply.github.com>
2024-02-23 11:58:17 +0800

    Delete week01-3.cpp

--- week01-3.cpp
+++
@@ -1,8 +0,0 @@
-/// week01-3.cpp 結合 Hello World 和 %d
-/// 在 CodeBlock裡, File-New-Empty 空白檔
-///再 Ctrl-S 存檔成 week01-3.cpp, 寫程式
-#include <stdio.h>
-int main()
-{
-    printf("Hello World %d\n", 100);
-}
