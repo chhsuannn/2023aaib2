e74017ce774509911e803874db08fad93bdd4221
chhsuannn <sandy100250@gmail.com>
2024-03-29 11:47:05 +0800

    week06

---
+++ week06/week06-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, n;
+	scanf("%d%d%d",&a,&b,&n);
+	printf("%d", a); //train head
+	for (int i=1; i<n; i++)
+	{
+		a +=b;
+		printf(",%d",a); //others
+	}
+}
